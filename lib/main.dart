55145e1134d934374739ccde219416d53dfad273
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2022-08-23 17:26:49 +0700

    If Statemen

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,21 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final questions = const [
+    {
+      "questionText": "What's your favorite color?",
+      "answer": ["Black", "Red", "Green", "White"]
+    },
+    {
+      "questionText": "What's your favorite animal?",
+      "answer": ["Cat", "Rabbit", "Fish", "Bird"]
+    },
+    {
+      "questionText": "Who's your favorite actor?",
+      "answer": ["Jackie Chan", "Iko", "Mat Dog"]
+    }
+  ];
+
   var _questionIndex = 0;
 
   void _answerQuestion() {
@@ -26,35 +41,24 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      {
-        "questionText": "What's your favorite color?",
-        "answer": ["Black", "Red", "Green", "White"]
-      },
-      {
-        "questionText": "What's your favorite animal?",
-        "answer": ["Cat", "Rabbit", "Fish", "Bird"]
-      },
-      {
-        "questionText": "Who's your favorite actor?",
-        "answer": ["Jackie Chan", "Iko", "Mat Dog"]
-      }
-    ];
-
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text("First App"),
         ),
-        body: Column(
-          children: [
-            Question(questions[_questionIndex]["questionText"] as String),
-            ...(questions[_questionIndex]['answer'] as List<String>)
-                .map((answer) {
-              return Answer(_answerQuestion, answer);
-            }).toList(),
-          ],
-        ),
+        body: _questionIndex < questions.length
+            ? Column(
+                children: [
+                  Question(questions[_questionIndex]["questionText"] as String),
+                  ...(questions[_questionIndex]['answer'] as List<String>)
+                      .map((answer) {
+                    return Answer(_answerQuestion, answer);
+                  }).toList(),
+                ],
+              )
+            : const Center(
+                child: Text("You did it!"),
+              ),
       ),
     );
   }
